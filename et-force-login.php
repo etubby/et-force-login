e7e4e0f5f11abe73d6b0152e968955ba3ce714f8
Eric <18406711+etubby@users.noreply.github.com>
2017-08-14 10:38:41 -0400

    ## Added - README.md - login_redirect filter to Actions() #47
    ## Changed
    - Version to 1.0.1

--- et-force-login.php
+++ et-force-login.php
@@ -1,15 +1,16 @@
 <?php
 
-/*
-Plugin Name: ET Force Login
-Plugin URI: http://erictubby.ca/
-Description: Forces all visitors to be logged in
-Version: 1.0
-Author: Eric Tubby
-Author URI: http://erictubby.ca/
-License: GPLv2 or later (license.txt)
-
-*/
+/**
+ *  Plugin Name: ET Force Login
+ *  Plugin URI: http://erictubby.ca/
+ *  Description: Forces all visitors to be logged in
+ *  Version: 1.0.1
+ *  Author: Eric Tubby
+ *  Author URI: http://erictubby.ca/
+ *  License: GPLv2 or later (license.txt)
+ *  GitHub Plugin URI: https://github.com/etubby/et-force-login
+ *
+ **/
 
 namespace ET_Force_Login;
 
